a084a007412427531add79cfbf044a11a8a22da7
StMannyAkinso <111481437+StMannyAkinso@users.noreply.github.com>
2024-09-06 09:36:54 +0100

    Delete Scripts directory

--- Scripts/Charts/map-chart.js
+++
@@ -1,4 +0,0 @@
-function MapChart({ jsonData }) {
-  // D3.js code for rendering map chart
-  return <div id="mapChart">{/* Map chart will be drawn here */}</div>;
-}
--- Scripts/Charts/multi-chart.js
+++
@@ -1,4 +0,0 @@
-function MultiChart({ jsonData }) {
-  // D3.js code for rendering multi-chart
-  return <div id="multiChart">{/* Multi-chart will be drawn here */}</div>;
-}
